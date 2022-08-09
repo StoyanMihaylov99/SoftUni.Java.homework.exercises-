66c221d771b55e038479a0a5fe513b3230051df5
Stoyan Mihaylov <107346999+StoyanMihaylov99@users.noreply.github.com>
2022-08-09 13:30:58 +0300

    Upload exercise

---
+++ Java.Basics/Data Types and Variables Exercise/Exact Sum of Real Numbers/README.md
@@ -0,0 +1,19 @@
+Exact Sum of Real Numbers
+
+
+
+
+Write a program to enter n numbers and calculate and print their exact sum (without rounding).
+
+
+
+Example:
+
+
+Input:                    Output:
+
+
+3                          1000000000000000015
+1000000000000000000 
+5
+10
---
+++ Java.Basics/Data Types and Variables Exercise/Exact Sum of Real Numbers/exactSumOfRealNumbers.java
@@ -0,0 +1,19 @@
+package dataTypes;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class exactSumOfRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        BigDecimal sum = new BigDecimal(0);
+
+        for (int i = 1; i <= n; i++) {
+            BigDecimal currentNum = new BigDecimal(scanner.nextLine());
+            sum = sum.add(currentNum);
+        }
+        System.out.println(sum);
+    }
+}
