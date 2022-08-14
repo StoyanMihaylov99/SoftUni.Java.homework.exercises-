d023f0fec0d0267990a08af4e975c7d862c7ee6f
Stoyan Mihaylov <107346999+StoyanMihaylov99@users.noreply.github.com>
2022-08-14 12:21:01 +0300

    Upload exercise

---
+++ Java.Basics/Data Types and Variables Exercise/Centuries to Minutes/README.md
@@ -0,0 +1,14 @@
+Centuries to Minutes:
+
+
+Write a program to enter an integer number of centuries and convert it to years, days, hours, and minutes.
+
+
+
+Example:
+ 
+Input:                 Output:
+
+
+ 5                       5 centuries = 500 years = 182621 days = 4382906
+                         hours = 262974384 minutes                     
---
+++ Java.Basics/Data Types and Variables Exercise/Centuries to Minutes/centuriesToMinutes.java
@@ -0,0 +1,19 @@
+package dataTypes;
+
+import java.util.Scanner;
+
+public class centuriesToMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        byte centuries = Byte.parseByte(scanner.nextLine());
+
+       int years = centuries * 100;
+       double days = 365.2422 * years;
+       double hours = days * 24;
+       double minutes = hours * 60;
+
+        System.out.printf("%d centuries = %d years = %.0f days = %.0f hours = %.0f minutes",centuries,years,days,hours,minutes);
+
+    }
+}
