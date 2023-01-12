c3d151a45e44207e969f1a5d44d81f353b1ad0ad
Stoyan Mihaylov <107346999+StoyanMihaylov99@users.noreply.github.com>
2023-01-12 20:53:18 +0200

    Upload exercise

---
+++ Java.Advanced/Stacks & Queues/Recursive Fibonacci/recursiveFibonacci.java
@@ -0,0 +1,29 @@
+package stacksAndQueues.Exercise;
+
+import java.util.Scanner;
+
+public class recursiveFibonacci {
+    public static long[] memory;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+        memory = new long[n + 1];
+        long result = fib(n);
+        System.out.println(result);
+
+
+    }
+
+    public static long fib(int n){
+        if(n < 2){
+            return 1;
+        }
+        if(memory[n] != 0){
+            return memory[n];
+        }
+        memory[n] = fib(n - 1) + fib(n - 2);
+        return memory[n];
+    }
+}
