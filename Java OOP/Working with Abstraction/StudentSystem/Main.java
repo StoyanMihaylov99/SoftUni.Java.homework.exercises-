9603d4e7ea2815d4b3fc80ced72f6c3e07d4ac26
Stoyan Mihaylov <107346999+StoyanMihaylov99@users.noreply.github.com>
2023-04-12 22:48:20 +0300

    Upload exercise

---
+++ Java OOP/Working with Abstraction/StudentSystem/Main.java
@@ -0,0 +1,20 @@
+package WorkingWithAbstraction.StudentSystem;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentSystem studentSystem = new StudentSystem();
+
+        while (true) {
+            String[] input = scanner.nextLine().split("\\s+");
+            if (input[0].equals("Exit")) {
+                break;
+            }
+
+            studentSystem.ParseCommand(input);
+        }
+    }
+}
