dc5ad7dc215a382dbbbcc804748f7824b44f8a5c
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-01 23:53:17 +0300

    Upload exercise

---
+++ Java OOP/Inheritance/Exercise/Person/Main.java
@@ -0,0 +1,14 @@
+package person;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Child child = new Child(name,age);
+    }
+}
