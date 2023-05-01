1033f367e6cd9a9f2ee268f7e9986e51f15eb446
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-01 23:51:15 +0300

    Upload exercise

---
+++ Java OOP/Inheritance/StackOfStrings/Main.java
@@ -0,0 +1,17 @@
+package StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+        StackOfStrings stackOfStrings = new StackOfStrings();
+        stackOfStrings.push("one");
+        stackOfStrings.push("tow");
+        stackOfStrings.push("three");
+
+        System.out.println(stackOfStrings.isEmpty());
+        System.out.println(stackOfStrings.peek());
+
+        System.out.println(stackOfStrings.pop());
+        System.out.println(stackOfStrings.pop());
+        System.out.println(stackOfStrings.pop());
+    }
+}
