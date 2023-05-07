eb8fb8385800d9470fba81589f22c5535b3a3f45
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-08 00:00:28 +0300

    Upload exercise

---
+++ Java OOP/Inheritance/Exercise/Restaurant/ColdBeverage.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage{
+
+    public ColdBeverage(String name, BigDecimal price, int milliliters){
+        super(name,price,milliliters);
+    }
+}
---
+++ Java OOP/Inheritance/Exercise/Restaurant/HotBeverage.java
@@ -0,0 +1,14 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+    public HotBeverage(String name, BigDecimal price, double milliliters){
+     super(name,price,milliliters);
+    }
+
+
+
+
+
+}
