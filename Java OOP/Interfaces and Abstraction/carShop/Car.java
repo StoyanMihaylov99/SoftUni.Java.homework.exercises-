422cc7da1d19c1f8f7e9a7a3c856c86f4ffc890b
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-09 19:31:41 +0300

    Upload exercise

---
+++ Java OOP/Interfaces and Abstraction/carShop/Car.java
@@ -0,0 +1,16 @@
+package carShop;
+
+import java.io.Serializable;
+
+    public interface Car extends Serializable {
+        Integer TIRES=4;
+
+
+        String getModel();
+        String getColor();
+        Integer getHorsePower();
+        String countryProduced();
+
+    }
+
+
