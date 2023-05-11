014c2e47c26f83d23d7b5249741ad12128c0be34
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-11 20:46:38 +0300

    Upload exercise

---
+++ Java OOP/Interfaces and Abstraction/sayHello/Chinese.java
@@ -0,0 +1,20 @@
+package sayHello;
+
+public class Chinese implements Person{
+    private String name;
+
+
+    public Chinese(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
