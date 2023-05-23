264fafa9dc37069b0a3ff6c8b55930eb0537d040
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-23 23:15:17 +0300

    Upload exercise

---
+++ Java OOP/Polymorphism/Animal/Dog.java
@@ -0,0 +1,11 @@
+package Animal;
+public class Dog extends Animal {
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "DJAAF";
+    }
+}
