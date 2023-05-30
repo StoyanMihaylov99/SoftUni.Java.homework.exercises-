5edfa4e2d0b8c770574ae3cfb2dbee1fc52b8754
Stoyan Mihaylov <stoqn.business@gmail.com>
2023-05-30 23:04:32 +0300

    Upload exercise

---
+++ Java OOP/Reflection/Reflection/Main.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Reflection> clazz = Reflection.class;
+        System.out.println(clazz);
+        System.out.println(clazz.getSuperclass());
+        Arrays.stream(clazz.getInterfaces()).forEach(System.out::println);
+        Constructor<Reflection> ctor = clazz.getDeclaredConstructor();
+        Reflection reflection = ctor.newInstance();
+        System.out.println(reflection);
+    }
+
+}
